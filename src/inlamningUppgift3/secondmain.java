58f66d16b4e51ea58036a79186faf0c87b7adac2
Sami000071 <149934137+Sami000071@users.noreply.github.com>
2023-11-22 14:46:06 +0100

    secondmain.java
    edited

--- src/inlamningUppgift3/secondmain.java
+++ src/inlamningUppgift3/secondmain.java
@@ -1,63 +1,41 @@
 package inlamningUppgift3;
 
-import java.util.Scanner;
-
-public class secondmain {
+public class secondMain {
     private int charCount = 0;
     private int lines = 0;
 
-    public void getinfo(String input) {
 
-        if (!input.equals("stop")) { // undvika ordet stop
-            for (int i = 0; i < input.length(); i++) { // för att iterera över varje tecken i strängen input.
-                if (input.charAt(i) != ' ') { // för att undvika spaces och kunna räkna tecken
-                    charCount++; // värje gång vi looper ett tecken så plusar med 1
+    public void getinfo(String input) {
+        if (!input.equals("stop")) {
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) != ' ') {
+                    charCount++;
                 }
 
 
+                if (input.charAt(i) == '\n') {// detta för att vi ska
+                    //kunna testa lines.
+                    lines++;
+                }
+
             }
 
-            lines++; // 1 + värje loop
+            lines++;
             System.out.println("charCount = " + charCount + " lines = " + lines);
-            //skrivs ut värje loop
         }
-
-
     }
 
-    public int getCharCount(){ // retunera båda ints för att kunna skriva ut dem
+
+    public int getCharCount() { // retunera båda ints för att kunna skriva ut dem
         return charCount;
     }
 
-    public int getLines(){
+    public int getLines() {
         return lines;
     }
-
-
-    //tests
-
-    public String getname() {
-        String name = "alex";
-        return name;
-    }
-
-    //
-    private int numr;
-
-    public int getNumr(int numr1) {
-        numr = numr1 + 10;
-        return numr;
-    }
-    //
-
-    private String[] groupp;
-    public void setGroupp(String[] theusers)
-    {groupp = theusers;}
-    public String[] getGroupp(){
-        return groupp;
-    }
-
 }
+
+
 
 
 

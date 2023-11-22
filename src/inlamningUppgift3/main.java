b2a455672e6a523ece05bb2e7e6a0cd86a4176a3
Sami000071 <149934137+Sami000071@users.noreply.github.com>
2023-11-22 14:45:26 +0100

    main.java
    edited

--- src/inlamningUppgift3/main.java
+++ src/inlamningUppgift3/main.java
@@ -11,13 +11,13 @@ import static org.junit.Assert.assertNotEquals;
 public class main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        secondmain secondmain = new secondmain();
+        secondMain secondMain = new secondMain();
 
         System.out.println("Write something to count / type stop to exit: ");
 
         while (true) {
             String input = scan.nextLine();
-            secondmain.getinfo(input);
+            secondMain.getinfo(input);
 
             if (input.equals("stop")) {
 
@@ -26,42 +26,8 @@ public class main {
             }
 
         }
-        System.out.println("total characters " + secondmain.getCharCount());
-        System.out.println("total lines " + secondmain.getLines());
+        System.out.println("total characters " + secondMain.getCharCount());
+        System.out.println("total lines " + secondMain.getLines());
     }
-
-
-    @Test
-    public void test1(){
-        secondmain secondmain = new secondmain();
-
-        String expected = "alex";
-
-        String actual = secondmain.getname();
-
-        assertNotEquals(expected, actual);
-
-    }
-
-    @Test
-    public void test2(){
-        secondmain secondmain = new secondmain();
-        int expected = 20;
-
-        int actual = secondmain.getNumr(10);
-
-        assertEquals(expected, actual);
-
-    }
-
-    @Test
-    public void test3(){
-        secondmain secondmain = new secondmain();
-        String[] expected = {"josh", "sam", "dan"};
-        secondmain.setGroupp(new String[]{"josh", "sam", "dan"});
-        String[] actual = secondmain.getGroupp();
-
-        assertEquals(expected, actual);
-    }
-
 }
+
